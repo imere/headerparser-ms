a381ca64413a5862a7de9f99c15a9ecfa5e5925d
imere <838914662@qq.com>
2018-05-09 14:13:38 +0800

    reserve general ip info

--- headerparser.js
+++ headerparser.js
@@ -3,11 +3,10 @@ const headerparser = require("express").Router();
 module.exports = headerparser;
 
 headerparser.get("/whoami", (req, res, next) => {
-	let ip = /^([\s\S]+):([\s\S]+)/.exec(req.headers["x-forwarded-for"] ||
+	let ip = req.headers["x-forwarded-for"] ||
 	req.connection.remoteAddress ||
 	req.socket.remoteAddress ||
-	req.connection.socket.remoteAddress);
-	ip = ip[ip.length-1];
+	req.connection.socket.remoteAddress;
 	let lang = req.headers["accept-language"].split(",")[0];
 	let soft = /\(([\s\S]+?)\)/.exec(req.headers["user-agent"])[1];
 	let rt = JSON.stringify({
