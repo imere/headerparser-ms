f90a885e4c05fe6857c9987f2f2a35ca51b4de1e
imere <838914662@qq.com>
2018-05-09 13:55:59 +0800

    test

--- headerparser.js
+++ headerparser.js
@@ -6,7 +6,7 @@ headerparser.get("/whoami", (req, res, next) => {
 	let ip = /*/^[\s\S]+:([\s\S]+)/.exec(*/req.headers["x-forwarded-for"] ||
 	req.connection.remoteAddress ||
 	req.socket.remoteAddress ||
-	req.connection.socket.remoteAddress);//[1];
+	req.connection.socket.remoteAddress;//);[1];
 	let lang = req.headers["accept-language"].split(",")[0];
 	let soft = /\(([\s\S]+?)\)/.exec(req.headers["user-agent"])[1];
 	let rt = JSON.stringify({
