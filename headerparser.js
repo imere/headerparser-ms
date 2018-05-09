6f3155eba45ea3c9e1a027aefd0dfd735183131e
imere <838914662@qq.com>
2018-05-09 13:24:03 +0800

    test

--- headerparser.js
+++ headerparser.js
@@ -3,14 +3,14 @@ const headerparser = require("express").Router();
 module.exports = headerparser;
 
 headerparser.get("/whoami", (req, res, next) => {
-	let ip = /^[\s\S]+:([\s\S]+)/.exec(req.headers["x-forwarded-for"] ||
+	//let ip = /^[\s\S]+:([\s\S]+)/.exec(req.headers["x-forwarded-for"] ||
 	req.connection.remoteAddress ||
 	req.socket.remoteAddress ||
 	req.connection.socket.remoteAddress)[1];
 	let lang = req.headers["accept-language"].split(",")[0];
 	let soft = /\(([\s\S]+?)\)/.exec(req.headers["user-agent"])[1];
 	let rt = JSON.stringify({
-		"ipaddress": ip,
+		//"ipaddress": ip,
 		"language": lang,
 		"software": soft
 	});
